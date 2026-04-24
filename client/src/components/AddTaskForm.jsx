1331be26baa4eb25d2467f26e27987f50eddccb6
orfneos <orfneos@yahoo.gr>
2026-04-25 02:13:49 +0300

    continue with Tailwind

--- client/src/components/AddTaskForm.jsx
+++ client/src/components/AddTaskForm.jsx
@@ -12,8 +12,19 @@ function handleSubmit() {
 
   return(
     <>
-     <input value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
-      <button onClick={handleSubmit}>Add</button>
+    <div className="flex gap-4">
+      <input 
+        value={newTask} 
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a new task..."
+        className="flex-1 px-4 py-3 rounded-xl bg-white text-black outline-none"
+        /> 
+
+        <button 
+        onClick={handleSubmit}
+        className="px-6 py-3 rounded-xl bg-black text-orange-400 font-semibold hover:opacity-80 transition shadow-xl"
+        >Add</button>
+    </div>
     </>
    
   )
